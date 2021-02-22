5cc34d206562eda1df02e79c9a207952cd28f1ec
h846 <h846@outlook.com>
2021-02-22 17:11:31 +0900

    create toollink child

--- pages/Admin/ToolLinks/_items.vue
+++ pages/Admin/ToolLinks/_items.vue
@@ -1,8 +1,86 @@
 <template>
-  <section>
-    {{ $route.params.items }}
-  </section>
+  <v-container>
+    <p>{{ $route.params.items }}</p>
+    <v-row>
+      <v-col>
+        <v-btn :disabled="btnDisable" @click="deleteItem" color="error">
+          <v-icon left>mdi-minus-circle</v-icon>
+          チェックした項目を削除
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="success">
+          <v-icon left>mdi-plus-circle</v-icon>
+          項目を追加
+        </v-btn>
+      </v-col>
+    </v-row>
+    <ul>
+      <li v-for="item in itemList" :key="item.ID">
+        <v-row align="center" justify="start">
+          <v-col>
+            <v-checkbox
+              v-model="checkedItems"
+              :label="item.Name"
+              :value="item.ID"
+            ></v-checkbox>
+          </v-col>
+          <v-col>
+            <a :href="item.Link" target="_blank">LINK</a>
+          </v-col>
+        </v-row>
+      </li>
+    </ul>
+  </v-container>
 </template>
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      dbdata: [],
+      catid: this.$route.query.catid,
+      checkedItems: [],
+      btnDisable: true,
+    };
+  },
+  created() {
+    let res = axios
+      .get("http://lejnet/API/accdb/", {
+        params: {
+          db: "CSNet/dataCenter/DB/Tool/tools_home.mdb",
+          table: "tool",
+        },
+      })
+      .then((res) => {
+        this.dbdata = res.data;
+      });
+  },
+  computed: {
+    itemList: function () {
+      return this.dbdata.filter((item) => {
+        return item.category == this.catid;
+      });
+    },
+  },
+  watch: {
+    checkedItems: function (val) {
+      if (val.length < 1) {
+        this.btnDisable = true;
+      } else {
+        this.btnDisable = false;
+      }
+    },
+  },
+  methods: {
+    deleteItem: function () {
+      alert(this.checkedItems);
+    },
+  },
+};
 </script>
+<style lang="stylus" scoped>
+ul {
+  list-style: none;
+}
+</style>
--- pages/Admin/ToolLinks/index.vue
+++ pages/Admin/ToolLinks/index.vue
@@ -13,7 +13,7 @@
                 </v-list-item-icon>
                 <v-list-item-content>
                   <v-list-item-title>
-                    <nuxt-link :to="'toollinks/' + item.name">
+                    <nuxt-link :to="`toollinks/${item.name}?catid=${item.ID}`">
                       {{ item.name }}
                     </nuxt-link>
                   </v-list-item-title>
